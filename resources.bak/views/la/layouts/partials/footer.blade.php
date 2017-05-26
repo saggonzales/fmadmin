7928038caa0cc9e1526d015588256b1b14909731
Sag Gonzales <sag.gonzales@gmail.com>
2017-05-26 19:14:40 +0800

    Remove backup directory

--- resources.bak/views/la/layouts/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-@if(!isset($no_padding))
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        Powered by <a href="http://movus.com.au" target="_blank">MOVUS</a>
-    </div>
-    <strong>Copyright &copy; {{ date('Y') }}
-</footer>
-@endif
